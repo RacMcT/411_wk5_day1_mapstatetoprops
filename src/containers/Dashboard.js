ac98944830bfecaf40baabaec1a60d2f0df9a4af
RacMcT <raccmct@gmail.com>
2021-09-01 18:30:44 -0500

    debugging dependencies with yarn

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,6 +1,6 @@
   
 import { connect } from "react-redux";
-import Dashboard from "./components/Dashboard.js";
+import Dashboard from "../components/Dashboard.js";
 
 const mapStateToProps = (state) => {
   return {
